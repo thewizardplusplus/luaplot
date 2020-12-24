ac9a576370cf8367eb8786a0dfb976f44d72cc86
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-24 23:00:00 +0300

    Add tests for the maths.random_in_range() function

--- maths_test.lua
+++ maths_test.lua
@@ -38,3 +38,30 @@ function TestMaths.test_clamp_after_maximum()
   luaunit.assert_is_number(result)
   luaunit.assert_equals(result, 42)
 end
+
+function TestMaths.test_random_in_range()
+  math.randomseed(1)
+
+  local results = {}
+  for _ = 1, 10 do
+    local result = maths.random_in_range(23, 42)
+    table.insert(results, result)
+  end
+
+  local wanted_results = {
+    30.493275606073,
+    37.878885244485,
+    38.170360628981,
+    40.321299792733,
+    26.753476014826,
+    29.369232355617,
+    37.596362294629,
+    28.277719502803,
+    33.525429155212,
+    32.070543981157,
+  }
+  luaunit.assert_equals(#results, #wanted_results)
+  for index, result in ipairs(results) do
+    luaunit.assert_almost_equals(result, wanted_results[index], 1e-6)
+  end
+end
