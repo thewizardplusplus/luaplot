1414816ab053736baf1c05ad32bf1ea75a7ebb29
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-30 23:00:00 +0300

    Add tests for the Oscillogram:update() method

--- oscillogram_test.lua
+++ oscillogram_test.lua
@@ -48,3 +48,51 @@ function TestOscillogram.test_new_partial()
   luaunit.assert_is_number(plot._maximum)
   luaunit.assert_equals(plot._maximum, 1)
 end
+
+function TestOscillogram.test_update_custom()
+  local plot = Oscillogram:new("custom", 0, 0.5)
+  for i = 1, 5 do
+    plot:push(i / 10)
+  end
+
+  local first_point = plot:update(0.2)
+
+  luaunit.assert_is_table(plot._points)
+  luaunit.assert_equals(plot._points, {0.2, 0.3, 0.4, 0.5, 0.2})
+
+  luaunit.assert_is_number(first_point)
+  luaunit.assert_equals(first_point, 0.1)
+end
+
+function TestOscillogram.test_update_linear()
+  local plot = Oscillogram:new("linear", 0, 0.5)
+  for i = 1, 5 do
+    plot:push(i / 10)
+  end
+
+  local first_point = plot:update(0.2)
+
+  luaunit.assert_is_table(plot._points)
+  luaunit.assert_equals(plot._points, {0.2, 0.3, 0.4, 0.5, 0.7})
+
+  luaunit.assert_is_number(first_point)
+  luaunit.assert_equals(first_point, 0.1)
+end
+
+function TestOscillogram.test_update_random()
+  math.randomseed(1)
+
+  local plot = Oscillogram:new("random", 0, 0.5)
+  for i = 1, 5 do
+    plot:push(i / 10)
+  end
+
+  local first_point = plot:update(0.2)
+
+  luaunit.assert_is_table(plot._points)
+  luaunit.assert_equals(#plot._points, 5)
+  luaunit.assert_almost_equals(plot._points[5], 0.457753, 1e-6)
+
+  luaunit.assert_is_number(first_point)
+  luaunit.assert_equals(first_point, 0.1)
+end
