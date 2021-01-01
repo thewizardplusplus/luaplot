f365bc35ccf3d9f443742e0e533828496fb81e9f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-01 23:00:00 +0300

    Add the PlotIteratorFactory:with() method

--- plotiteratorfactory.lua
+++ plotiteratorfactory.lua
@@ -3,6 +3,8 @@
 
 local middleclass = require("middleclass")
 local types = require("luaplot.types")
+local Plot = require("luaplot.plot")
+local PlotIterator = require("luaplot.plotiterator")
 
 ---
 -- @table instance
@@ -18,6 +20,15 @@ function PlotIteratorFactory:initialize(transformer)
   assert(types.is_callable(transformer))
 
   self._transformer = transformer
+end
+
+---
+-- @tparam Plot plot
+-- @treturn PlotIterator
+function PlotIteratorFactory:with(plot)
+  assert(types.is_instance(plot, Plot))
+
+  return PlotIterator:new(plot, self._transformer)
 end
 
 return PlotIteratorFactory
