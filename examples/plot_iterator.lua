b7ecb85692c63fee7379be9985e27ad4d899b411
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-02 23:00:00 +0300

    Add the example with the PlotIterator class

---
+++ examples/plot_iterator.lua
@@ -0,0 +1,38 @@
+local inspect = require("inspect")
+local types = require("luaplot.types")
+local Plot = require("luaplot.plot")
+local PlotIterator = require("luaplot.plotiterator")
+
+local function print_iterable(iterable)
+  local ipairs_metamethod = _VERSION >= "Lua 5.3" and "__index" or "__ipairs"
+  assert(types._has_metamethod(iterable, ipairs_metamethod))
+
+  local points = {}
+  for _, point in ipairs(iterable) do
+    table.insert(points, point)
+  end
+
+  print(inspect(points))
+end
+
+local plot = Plot:new(0)
+for i = 1, 5 do
+  plot:push(i / 10)
+end
+print_iterable(plot)
+
+local iterator_one = PlotIterator:new(plot, function(index, point)
+  assert(types.is_number_with_limits(index, 1))
+  assert(types.is_number_with_limits(point))
+
+  return point * index
+end)
+print_iterable(iterator_one)
+
+local iterator_two = PlotIterator:new(plot, function(index, point)
+  assert(types.is_number_with_limits(index, 1))
+  assert(types.is_number_with_limits(point))
+
+  return {x = index, y = point}
+end)
+print_iterable(iterator_two)
