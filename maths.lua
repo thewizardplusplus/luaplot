474809c608fcae1b7b6b61461fb8cb0cfd18f8cd
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-01-05 23:00:00 +0300

    Add the maths.lerp() function

--- maths.lua
+++ maths.lua
@@ -27,6 +27,19 @@ end
 ---
 -- @tparam number minimum
 -- @tparam number maximum [minimum, ∞)
+-- @tparam number progress [0, 1]
+-- @treturn number [minimum, maximum]
+function maths.lerp(minimum, maximum, progress)
+  assert(types.is_number_with_limits(minimum))
+  assert(types.is_number_with_limits(maximum, minimum))
+  assert(types.is_number_with_limits(progress, 0, 1))
+
+  return (maximum - minimum) * progress + minimum
+end
+
+---
+-- @tparam number minimum
+-- @tparam number maximum [minimum, ∞)
 -- @treturn number [minimum, maximum)
 function maths.random_in_range(minimum, maximum)
   assert(types.is_number_with_limits(minimum))
