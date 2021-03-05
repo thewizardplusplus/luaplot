619484ae1a1881771a10793c6e46b6d96323e39a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-05 23:00:00 +0300

    Add a base of the example with distance detection

---
+++ examples/distance_detection.lua
@@ -0,0 +1,50 @@
+local types = require("luaplot.types")
+local Plot = require("luaplot.plot")
+local Oscillogram = require("luaplot.oscillogram")
+
+local function print_plot(plot, vertical_step)
+  assert(types.is_instance(plot, Plot))
+  assert(types.is_number_with_limits(vertical_step, 0, 1))
+
+  local text = ""
+  for height = 1, 0, -vertical_step do
+    for _, point in ipairs(plot) do
+      local delta = math.abs(point - height)
+      local symbol = delta < vertical_step / 2 and "*" or "."
+      text = text .. symbol
+    end
+
+    text = text .. "\n"
+  end
+
+  io.write(text)
+end
+
+local function sleep(seconds)
+  assert(types.is_number_with_limits(seconds, 0))
+
+  local start = os.clock()
+  while os.clock() - start < seconds do end
+end
+
+local SPEED = 0.25
+local WIDTH = 40
+local HEIGHT = 5
+local VERTICAL_STEP = 1 / HEIGHT
+local PRINT_DELAY = 1 / (SPEED * WIDTH)
+local FACTOR = 1.5 * PRINT_DELAY
+
+math.randomseed(os.time())
+
+local plot_one = Oscillogram:new("random", WIDTH, 0.5)
+local plot_two = Oscillogram:new("random", WIDTH, 0.5)
+while true do
+  plot_one:update(FACTOR)
+  plot_two:update(FACTOR)
+
+  print_plot(plot_one, VERTICAL_STEP)
+  print_plot(plot_two, VERTICAL_STEP)
+  io.write("\n")
+
+  sleep(PRINT_DELAY)
+end
