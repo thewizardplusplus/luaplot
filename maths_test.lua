7363c79561a3700e75f3dec7a2dcc9fd33305668
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-12-18 23:20:40 +0600

    Revert "Use the `cpml.utils.clamp()` function"
    This reverts commit 6a4ce003abe6703cbc998c8a977fd1a956b7016e.

--- maths_test.lua
+++ maths_test.lua
@@ -4,6 +4,41 @@ local maths = require("luaplot.maths")
 -- luacheck: globals TestMaths
 TestMaths = {}
 
+function TestMaths.test_clamp_middle()
+  local result = maths.clamp(32, 23, 42)
+
+  luaunit.assert_is_number(result)
+  luaunit.assert_equals(result, 32)
+end
+
+function TestMaths.test_clamp_minimum()
+  local result = maths.clamp(23, 23, 42)
+
+  luaunit.assert_is_number(result)
+  luaunit.assert_equals(result, 23)
+end
+
+function TestMaths.test_clamp_before_minimum()
+  local result = maths.clamp(22, 23, 42)
+
+  luaunit.assert_is_number(result)
+  luaunit.assert_equals(result, 23)
+end
+
+function TestMaths.test_clamp_maximum()
+  local result = maths.clamp(42, 23, 42)
+
+  luaunit.assert_is_number(result)
+  luaunit.assert_equals(result, 42)
+end
+
+function TestMaths.test_clamp_after_maximum()
+  local result = maths.clamp(43, 23, 42)
+
+  luaunit.assert_is_number(result)
+  luaunit.assert_equals(result, 42)
+end
+
 function TestMaths.test_lerp_middle()
   local result = maths.lerp(23, 42, 0.2)
 
