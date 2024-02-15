4623779e3aa3c0ac54801f2f6f000f1d78b702b3
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-02-15 20:51:36 +0600

    Use the `luatypechecks` package in the `Iterable` class

--- iterable.lua
+++ iterable.lua
@@ -1,7 +1,7 @@
 ---
 -- @classmod Iterable
 
-local types = require("luaplot.types")
+local assertions = require("luatypechecks.assertions")
 local iterators = require("luaplot.iterators")
 
 local Iterable = {}
@@ -12,7 +12,7 @@ local Iterable = {}
 -- @treturn Iterable self
 -- @treturn number always zero
 function Iterable:__ipairs()
-  assert(types.is_indexable(self))
+  assertions.has_metamethods(self, {"__index"})
 
   return iterators.inext, self, 0
 end
